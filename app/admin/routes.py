a16a791e6544654a4106fa6a7c212e99978bf0df
Landerer0 <lucaskraemera@gmail.com>
2022-07-08 13:25:59 -0400

    volviendo a e4956c9

---
+++ app/admin/routes.py
@@ -0,0 +1,234 @@
+from flask import render_template, url_for, request, jsonify,abort,current_app
+from sqlalchemy import true
+from flask_login import current_user, login_required
+from app.auth.routes import admin_required
+import json
+from app.mail import send_email
+from collections import Counter
+from app import admin, db
+from . import admin_bp
+from .forms import MailForm
+import re
+
+@admin_bp.route('/crearEncuesta')
+@login_required
+@admin_required
+def rutaCrearEncuesta():
+    return render_template('admin/crearEncuesta.html')
+
+@admin_bp.route('/guardarEncuesta', methods=['POST'] )
+@login_required
+@admin_required
+def guardar_encuesta():
+    print("guardar encuesta")
+    if request.method == 'POST':
+        datosEncuesta = request.get_json(force = True)
+        titulo=datosEncuesta[0]
+        descripcion=datosEncuesta[1]
+        fechaComienzo=datosEncuesta[2]
+        fechaTermino=datosEncuesta[3]
+        preguntas=datosEncuesta[4]
+        numPreguntas=len(preguntas)
+
+        print(datosEncuesta)
+
+        try:
+            sql = 'INSERT INTO encuesta (id_encuesta, titulo_encuesta, descripcion,fecha_comienzo,fecha_termino,preguntas[%s]) VALUES (DEFAULT,%s,%s,%s,%s,%s);'
+            db.connect()
+            db.execute(sql, (numPreguntas,titulo,descripcion,fechaComienzo,fechaTermino,json.dumps(preguntas)))
+            print("se ejecuto consulta SQL para guardar encuesta")
+            sentenciaSQL = 'SELECT correo FROM mails WHERE mails.suscrito = True;'
+            todos_correos = db.fetch_all(sentenciaSQL)
+            todos_correos = [x[0] for x in todos_correos]
+            sentsql = 'SElECT MAX(id_encuesta) FROM encuesta;'
+            id_encuesta= db.fetch_one(sentsql)
+            db.close()
+            for i in todos_correos:
+                send_email(subject='Encuesta para responder',
+                       sender=current_app.config['DONT_REPLY_FROM_EMAIL'],
+                       recipients=[i],
+                       text_body='Hola, puedes contestar la encuesta entrando en: http://127.0.0.1:5004/showSurvey/'+str(id_encuesta[0]),
+                       html_body=None)
+            return {"hola": "mundo!"}
+        except Exception as e:
+            print(e)
+
+    return {"hola": "mundo!"}
+
+@admin_bp.route('/guardarEditEncuesta', methods=['POST'] )
+@login_required
+@admin_required
+def guardar_editar_encuesta():
+    if request.method == 'POST':
+        datosEncuesta = request.get_json(force = True)
+        titulo=datosEncuesta[0]
+        descripcion=datosEncuesta[1]
+        fechaComienzo=datosEncuesta[2]
+        fechaTermino=datosEncuesta[3]
+        id=datosEncuesta[4]
+
+        try: 
+            sql = 'UPDATE encuesta SET titulo_encuesta = %s , descripcion = %s,fecha_comienzo = %s,fecha_termino = %s WHERE id_encuesta = %s'
+            db.connect()
+            db.execute(sql, (titulo,descripcion,fechaComienzo,fechaTermino,id))
+            db.close()
+            return {"hola": "mundo!"}
+        except Exception as e:
+            print(e)
+
+    return {"hola": "mundo!"}    
+
+@admin_bp.route('/editarEncuesta/<string:id>/')
+@login_required
+@admin_required
+def rutaEditarEncuesta(id):
+    sentenciaSQL = '''\
+    SELECT * FROM encuesta WHERE encuesta.id_encuesta = %s;'''
+    db.connect()
+    db_data = db.fetch_all(sentenciaSQL,(str(id),))[0]
+    db.close()
+
+    if db_data is None:
+        abort(404)
+
+    return render_template('admin/editarEncuesta.html', db_data=db_data)
+
+@admin_bp.route('/verEncuestas',methods=['GET', 'POST'])
+@login_required
+@admin_required
+def rutaDesplegarEncuestas():
+    db_data = None
+    try:
+        sentenciaSQL = '''\
+        SELECT encuesta.id_encuesta,encuesta.titulo_encuesta FROM encuesta ORDER BY encuesta.id_encuesta ASC'''
+        db.connect()
+        db_data = db.fetch_all(sentenciaSQL,)
+        db.close()
+    except Exception as e:
+            print(e)
+    if db_data is None:
+        abort(404)
+    return render_template('admin/desplegarMisEncuestas.html', db_data=db_data)
+
+@admin_bp.route("/verEncuestas/<string:id>/")
+@login_required
+@admin_required
+def mostrar_preguntas_alternativas(id):
+    sentenciaSQL = '''\
+    SELECT * FROM encuesta WHERE encuesta.id_encuesta = %s;'''
+    db.connect()
+    db_data = db.fetch_all(sentenciaSQL,(str(id),))[0]
+    db.close()
+
+    if db_data is None:
+        abort(404)
+    return render_template('admin/desplegarVerEncuesta.html',db_data=db_data)
+
+@admin_bp.route('/obtener_respuestas')
+@login_required
+@admin_required
+def obtener_respuestas():
+    print("estoy obteniendo las respuestas")
+    id_encuesta = request.args.get('id_encuesta')
+    print(id_encuesta)
+
+    db.connect()
+    sentenciaSQL = 'SELECT respuesta.respuestas FROM respuesta WHERE respuesta.id_encuesta = %s;'
+    todas_respuestas = db.fetch_all(sentenciaSQL,(str(id_encuesta),))
+
+    sentenciaSQL = 'SELECT encuesta.preguntas FROM encuesta WHERE encuesta.id_encuesta = %s;'
+    todas_preguntas = db.fetch_one(sentenciaSQL,(str(id_encuesta),))
+    db.close()
+
+    if(len(todas_respuestas) == 0):
+        return jsonify({'porcentajes':'No hay respuestas'})
+
+    #print("todas las respuestas:",todas_respuestas) #POSIBLE OPTIMIZACION
+    #print("todas las preguntas",todas_preguntas[0][0][0])
+    #respuestas = [item[4]['Respuestas'] for item in todas_respuestas]
+    respuestas = [item[0]['Respuestas'] for item in todas_respuestas]
+    n_preguntas = len(respuestas[0])
+    porcentajes = []
+    numero_de_respuestas = []
+    
+    for i in range(n_preguntas):
+        numero_de_respuestas_i = []
+        porcentajes_i = []
+        respuesta_i = [x[i] for x in respuestas]
+        count = Counter(respuesta_i)
+        total = sum(count.values())
+        for i in range(len(todas_preguntas[0][0][i]['Alternativas'])):
+            numero_de_respuestas_i.append(count[str(i+1)])
+            #print("counter",count[str(i+1)])
+            porcentajes_i.append(count[str(i+1)] / total * 100)
+        
+        numero_de_respuestas.append(numero_de_respuestas_i)
+        porcentajes.append(porcentajes_i)
+    #print(porcentajes)
+    
+    return jsonify({'porcentajes':porcentajes, 'n_respuestas':numero_de_respuestas}) #devuelve el porcentaje
+
+@admin_bp.route('/agregarmails',methods=['GET','POST'])
+@login_required
+@admin_required
+def insertarmail():
+    form=MailForm()
+    error=None
+    creado=None
+    if request.method=='POST':
+        if form.validate_on_submit():
+            email=form.email.data
+            if form.submit.data:
+                db.connect()
+                correos=db.fetch_all('SELECT * FROM mails ORDER BY correo ASC')
+                correo=db.fetch_one('SELECT * FROM mails where correo=%s',(email,))
+                db.close()
+                if correo != None:
+                    if correo[1] == True:
+                        error= f'Mail ya existente en la base de datos'
+                        return render_template('admin/agregarmails.html',form=form,error=error,db_data=correos)
+                    else:
+                        suscrito= True
+                        db.connect()
+                        db.execute('UPDATE mails SET suscrito=%s where correo=%s',(suscrito,correo[0]))
+                        creado= f'Mail resuscrito exitosamente'
+                        correos=db.fetch_all('SELECT * FROM mails ORDER BY correo ASC')
+                        db.close()
+                        return render_template('admin/agregarmails.html', form=form,creado=creado,db_data=correos)
+                suscrito= True
+                db.connect()
+                db.execute('INSERT INTO mails values(%s,%s)',(email,suscrito))
+                creado= f'Mail ingresado exitosamente'
+                correos=db.fetch_all('SELECT * FROM mails ORDER BY correo ASC')
+                db.close()
+                return render_template('admin/agregarmails.html', form=form,creado=creado,db_data=correos)
+            else:
+                email=form.email.data
+                suscrito= False
+                db.connect()
+                correo=db.fetch_one('SELECT * FROM mails where correo=%s',(email,))
+                correos=db.fetch_all('SELECT * FROM mails ORDER BY correo ASC')
+                db.close()
+                if correo==None:
+                    error=f'Correo no existente en la base de datos, puedes agregarlo'
+                    return render_template('admin/agregarmails.html', form=form,error=error,db_data=correos)
+                if correo[1]==True:
+                    db.connect()
+                    db.execute('UPDATE mails SET suscrito=%s where correo=%s',(suscrito,email))
+                    creado= f'Mail desuscrito exitosamente'
+                    correos=db.fetch_all('SELECT * FROM mails')
+                    db.close()
+                    return render_template('admin/agregarmails.html', form=form,creado=creado,db_data=correos)
+                else:
+                    error= f'Mail ya desuscrito, verifique los datos'
+                    return render_template('admin/agregarmails.html', form=form,error=error,db_data=correos)
+        else:
+            error= f'Datos incorrectos,intente de nuevo'
+            db.connect()
+            correos=db.fetch_all('SELECT * FROM mails ORDER BY correo ASC')
+            db.close()
+            return render_template('admin/agregarmails.html', form=form,error=error,db_data=correos)
+    db.connect()
+    correos=db.fetch_all('SELECT * FROM mails ORDER BY correo ASC')
+    db.close()
+    return render_template('admin/agregarmails.html', form=form,error=error,db_data=correos)
---
+++ app/admin/static/js/script.js
@@ -0,0 +1,273 @@
+//let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; //same as import
+var numberOfQuestion = 0;
+
+// FALTA IMPLEMENTAR BOTON PARA ELIMINAR PREGUNTAS Y BOTON PARA ELIMINAR ALTERNATIVAS
+// FALTA INDICAR EL NUMERO DE LA PREGUNTA
+function addTitulo(){
+    var container = document.getElementById("containerTitulo") // la div class
+    // generacion de un campo input (Para asignar titulo) 
+	container.titulo = document.createElement("input") // se refiere a un h1, un div, un boton, un elemento HTML
+    container.titulo.id = "titleId"
+    container.titulo.placeholder = "Agregue el Título aquí" // place holder
+    container.titulo.className = "form-control form-control-lg"
+    
+    container.descripcion = document.createElement("input") // se refiere a un h1, un div, un boton, un elemento HTML
+    container.descripcion.id = "descripcionId"
+    container.descripcion.placeholder = "Agregue una Descripción aquí" // place holder
+    container.descripcion.className = "form-control"
+
+    container.fechaComienzo = document.createElement("input")
+    container.fechaComienzo.id= "fechaComienzoId"
+    container.fechaComienzo.type= "date"
+    container.fechaComienzo.className = "me-5"
+    container.fechaTermino = document.createElement("input")
+    container.fechaTermino.id= "fechaTerminoId"
+    container.fechaTermino.type= "date"
+
+    const titulo = document.createElement("h1");
+    titulo.textContent = "Título";
+    const descripcion = document.createElement("h3");
+    descripcion.textContent = "Descripción";
+    container.appendChild(titulo/*document.createTextNode("Titulo:")*/)
+    container.appendChild(container.titulo)
+    container.appendChild(document.createElement("br")) // br es un salto de linea
+    container.appendChild(descripcion)
+    container.appendChild(container.descripcion)
+    container.appendChild(document.createElement("br")) // br es un salto de linea
+    container.appendChild(document.createElement("br")) // br es un salto de linea
+    container.appendChild(document.createTextNode("Fecha Comienzo:"))
+    container.appendChild(container.fechaComienzo)
+    container.appendChild(document.createTextNode("   Fecha Termino:"))
+    container.appendChild(container.fechaTermino)
+}
+
+function addAlternativa(pregunta){
+    //containerAlternativa.appendChild(document.createElement("&nbsp"))
+    // generacion de un campo input (la pregunta en si) 
+
+    var alternativa = document.createElement("div")
+
+	alternativa.contenido = document.createElement("input") // se refiere a un h1, un div, un boton, un elemento HTML
+    alternativa.contenido.id = "alternativaId" + pregunta.numAlternativas;
+    alternativa.contenido.placeholder = "Agregue una Alternativa aquí" // place holder
+    alternativa.contenido.name = "input" + pregunta.numAlternativas;
+    alternativa.contenido.className="form-control form-control-sm col-6"
+
+    alternativa.boton = document.createElement('button');
+    alternativa.boton.className = "btn btn-outline-dark m-2"
+    alternativa.boton.innerHTML = "- alternativa"
+    alternativa.boton.addEventListener('click', function(){
+        // funcion que elimina la alternativa
+        if(pregunta.numAlternativas<=2){
+            alert("Se tiene que tener un minimo de dos alternativas por pregunta")
+        }
+        else{
+            pregunta.alternativas.removeChild(alternativa)
+            pregunta.numAlternativas--    
+        }
+        
+    });
+
+    alternativa.appendChild(alternativa.contenido)
+    alternativa.appendChild(alternativa.boton)
+    alternativa.appendChild(document.createElement("br"))
+
+    pregunta.numAlternativas++
+    pregunta.alternativas.appendChild(alternativa)
+}
+
+// genera nuevo campo dinamico al apretar "mas" o "agrega otro campo de pregunta"
+function addEncuesta(){
+    var encuesta = document.getElementById("containerEncuesta")
+    var listPregunta = document.createElement("ul")
+    listPregunta.style = "padding-left: 0!important;"
+    encuesta.listPreguntas = listPregunta
+    encuesta.numPreguntas=0
+
+    encuesta.appendChild(encuesta.listPreguntas)
+}
+
+function addQuestion(){
+    var encuesta = document.getElementById("containerEncuesta")
+    encuesta.numPreguntas++
+    
+    var pregunta = document.createElement("div")
+
+    pregunta.contenidoPregunta = document.createElement("input")
+    pregunta.contenidoPregunta.name = "input" + numberOfQuestion
+    pregunta.contenidoPregunta.id = "inputId" + numberOfQuestion
+    pregunta.contenidoPregunta.placeholder = "Agregue una Pregunta aquí"
+    pregunta.contenidoPregunta.className = "form-control"
+    //pregunta.revisarContenidoPregunta = document.createElement("div")
+    //pregunta.revisarContenidoPregunta.appendChild(document.createTextNode("Vacio"))
+    //if(!pregunta.contenidoPregunta){
+    //    pregunta.revisarContenidoPregunta.style.display="block";
+    //}
+
+    pregunta.alternativas = document.createElement("ul")
+    pregunta.alternativas.style = "padding: 0px 50px!important;"
+    pregunta.numAlternativas = 0
+
+    pregunta.botonAlternativas = document.createElement('button');
+    pregunta.botonAlternativas.innerHTML = "+ alternativa"
+    pregunta.botonAlternativas.className = "btn btn-outline-dark m-2"
+    pregunta.botonAlternativas.addEventListener('click', function(){
+        addAlternativa(pregunta);
+    });
+
+    pregunta.botonPregunta = document.createElement('button');
+    pregunta.botonPregunta.innerHTML = "- pregunta"
+    pregunta.botonPregunta.className = "btn btn-outline-dark m-2"
+    pregunta.botonPregunta.addEventListener('click', function(){
+        // funcion que elimina la pregunta
+        encuesta.numPreguntas--
+        encuesta.listPreguntas.removeChild(pregunta)
+    });
+
+    pregunta.appendChild(document.createElement("br"))
+    pregunta.appendChild(pregunta.contenidoPregunta)
+    pregunta.appendChild(pregunta.botonPregunta)
+    pregunta.appendChild(pregunta.botonAlternativas)
+    //pregunta.appendChild(pregunta.revisarContenidoPregunta)
+    pregunta.appendChild(pregunta.alternativas)
+
+    encuesta.listPreguntas.appendChild(pregunta)
+
+    addAlternativa(pregunta)
+    addAlternativa(pregunta)
+
+    numberOfQuestion++;
+}
+
+const fetchData = (url_api,datosEncuesta) => {
+    
+    return new Promise((resolve, reject) => { // resolve se ejecuta cuando la peticion fue exitosa (reject cuando no)
+
+        const xhttp = new XMLHttpRequest(); // instanciacion
+
+        xhttp.open('POST', url_api , true); // "vamos al url de tipo post para ejecutar de forma asincrona"
+
+        xhttp.onreadystatechange = () => {
+            if(xhttp.readyState === 4){
+                if(xhttp.status === 200){
+                    resolve(JSON.parse(xhttp.responseText));
+                }else{
+                    reject(new Error('Error : ' + url_api));
+                }
+            }
+        }
+
+        xhttp.send(JSON.stringify(datosEncuesta));
+    });
+}
+
+const fetchDataAsync = async(url_api,datosEncuesta) => {
+    try {
+        const response = await fetchData(url_api,datosEncuesta);
+        
+        console.log(response)
+        alert("Encuesta " + datosEncuesta[0] +" enviada con exito")
+        window.location.replace("http://127.0.0.1:5004/");
+    } catch (error) {
+        console.error(error.message);
+        alert("Ocurrio un error, vuelva a intentarlo mas tarde")
+    }
+}
+
+function saveQuestions(){
+    var container = document.getElementById("containerTitulo")
+    var encuesta = document.getElementById("containerEncuesta")
+
+    var datosEncuesta = []
+    var jsonEncuesta = []
+
+    var titulo = container.titulo.value
+    var descripcion = container.descripcion.value
+    var fechaComienzo = container.fechaComienzo.value
+    var fechaTermino = container.fechaTermino.value
+
+    if(!titulo){
+        alert("Inserte un Titulo a la encuesta")
+        return
+    }
+    datosEncuesta.push(titulo)
+    if(!descripcion){
+        alert("Inserte una Descripción a la encuesta")
+        return
+    }
+    datosEncuesta.push(descripcion)
+    if(!fechaComienzo){
+        alert("Designe una fecha de comienzo a la encuesta")
+        return
+    }
+    datosEncuesta.push(fechaComienzo)
+    if(!fechaTermino){
+        alert("Designe una fecha de termino a la encuesta")
+        return
+    }
+    let today = new Date().toISOString().slice(0, 10)
+    //console.log(today);
+    //console.log(fechaComienzo);
+    if(fechaComienzo < today){
+        alert("Inserte una fecha de Comienzo desde hoy")
+        return
+    }
+    if(fechaComienzo>fechaTermino){
+        alert("La fecha de termino tiene que ser el mismo dia que la fecha de comienzo o posterior")
+        return
+    }
+    datosEncuesta.push(fechaTermino)
+    
+
+    //console.log(datosEncuesta);
+
+    // Falta ingresar formato para tener en JSON la encuesta
+    var listaPreguntas = encuesta.listPreguntas
+    if(encuesta.numPreguntas==0){
+        alert("Agregue al menos una pregunta a la encuesta")
+        return
+    }
+    var preguntaActual = 0
+    var contadorDivs = 0 // este es el contador que iterara por listaPreguntas
+    while(preguntaActual<encuesta.numPreguntas){
+        //console.log(listaPreguntas.getElementsByTagName("div")[contadorDivs])
+        //console.log(preguntaActual);
+        //console.log(listaPreguntas.getElementsByTagName("div")[contadorDivs].contenidoPregunta.value)
+        //console.log(listaPreguntas.getElementsByTagName("div")[contadorDivs].numAlternativas)
+        var json = {}
+        var alternativasDePregunta = []
+        var contenidoDePregunta = listaPreguntas.getElementsByTagName("div")[contadorDivs].contenidoPregunta.value
+        if(!contenidoDePregunta){
+            alert("inserte el enunciado de la pregunta " + (preguntaActual+1))
+            return;
+        }
+        json['Pregunta'] = contenidoDePregunta
+        var alternativaActual = 1
+        var numAlternativasActual = listaPreguntas.getElementsByTagName("div")[contadorDivs].numAlternativas
+        if(numAlternativasActual==0){
+            alert("La pregunta " + (preguntaActual+1) + " no tiene alternativas")
+            return
+        }
+        while(alternativaActual <= numAlternativasActual){
+            contenidoAlternativa = listaPreguntas.getElementsByTagName("div")[contadorDivs+alternativaActual].contenido.value
+            if(!contenidoAlternativa){
+                alert("La pregunta " + (preguntaActual+1) + " no tiene contenido en la alternativa " + alternativaActual)
+                return
+            }
+            alternativasDePregunta.push(contenidoAlternativa)
+            alternativaActual++
+        }
+        json['Alternativas'] = alternativasDePregunta
+        jsonEncuesta.push(json)
+        contadorDivs += listaPreguntas.getElementsByTagName("div")[contadorDivs].numAlternativas
+        contadorDivs++
+        preguntaActual++
+    }
+    //console.log(jsonEncuesta);
+    datosEncuesta.push(jsonEncuesta)
+
+    console.log(datosEncuesta);
+    
+    var url_api = "http://127.0.0.1:5004/guardarEncuesta"
+    fetchDataAsync(url_api, datosEncuesta);
+}
